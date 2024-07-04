9fe3dd5cb13ab3c8d6b1df29b239716e2e64fc69
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-07-04 10:11:57 +0700

    Add AuthController for handling authentication requests and token generation

---
+++ facebook-spring/src/main/java/com/facebook/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.facebook.controller;
+
+
+import com.facebook.config.auth.AuthService;
+import com.facebook.config.auth.AuthUser;
+import com.facebook.dto.AuthDTO;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@Validated
+@RequiredArgsConstructor
+public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    private final AuthService authService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthDTO.LoginRequest userLogin) throws IllegalAccessException {
+        Authentication authentication =
+                authenticationManager
+                        .authenticate(new UsernamePasswordAuthenticationToken(
+                                userLogin.username(),
+                                userLogin.password()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        AuthUser userDetails = (AuthUser) authentication.getPrincipal();
+
+
+        log.info("Token requested for user :{}", authentication.getAuthorities());
+        String token = authService.generateToken(authentication);
+
+        AuthDTO.Response response = new AuthDTO.Response("User logged in successfully", token);
+
+        return ResponseEntity.ok(response);
+    }
+}
