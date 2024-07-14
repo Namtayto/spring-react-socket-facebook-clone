cd6b2e8ee722dce84cc612922f3d3fc106af536a
Nguyễn Hoàng Nam <namnh.uit@gmail.com>
2024-07-14 17:15:15 +0700

    Edit MethodArgumentNotValidException handler in GlobalException to get more fields

--- spring-social/src/main/java/com/social/exception/GlobalException.java
+++ spring-social/src/main/java/com/social/exception/GlobalException.java
@@ -4,31 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalException {
-    @ExceptionHandler(UserException.class)
-    public ResponseEntity<ErrorDetail> UserExceptionHandle(UserException e, WebRequest req) {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDetail> UserExceptionHandle(UserNotFoundException e, WebRequest req) {
         ErrorDetail errorDetail = new ErrorDetail(e.getMessage(), req.getDescription(false), LocalDateTime.now());
-        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MessageException.class)
-    public ResponseEntity<ErrorDetail> MessageExceptionHandle(MessageException e, WebRequest req) {
-        ErrorDetail errorDetail = new ErrorDetail(e.getMessage(), req.getDescription(false), LocalDateTime.now());
-        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDetail> MethodArgumentNotValidExceptionHandle(MethodArgumentNotValidException e, WebRequest req) {
-        String error = e.getBindingResult().getFieldError().getDefaultMessage();
-        ErrorDetail errorDetail = new ErrorDetail("Validation Error", error, LocalDateTime.now());
-        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetail, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
@@ -41,5 +31,20 @@ public class GlobalException {
     public ResponseEntity<ErrorDetail> OtherExceptionHandler(Exception e, WebRequest req) {
         ErrorDetail errorDetail = new ErrorDetail(e.getMessage(), req.getDescription(false), LocalDateTime.now());
         return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ApiResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+                    if (errors.containsKey(error.getField())) {
+                        errors.put(error.getField(), String.format("%s, %s", errors.get(error.getField()), error.getDefaultMessage()));
+                    } else {
+                        errors.put(error.getField(), error.getDefaultMessage());
+                    }
+                }
+        );
+        return new ApiResponse(errors, "VALIDATION_FAILED");
     }
 }
